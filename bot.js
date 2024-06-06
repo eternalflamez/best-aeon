a00d422562f45034ad57611c6e5fb68b895e75bc
sandy <san_dman@hotmail.com>
2024-06-06 21:01:07 +0200

    Add port watching

--- bot.js
+++ bot.js
@@ -24,3 +24,17 @@ client.on('messageCreate', (message) => {
 })
 
 client.login(TOKEN)
+
+// Totally inconspicuous hello world website
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
