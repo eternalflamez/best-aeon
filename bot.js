5e52b4dd8222e49814fcc9e4a863b1f3c031a47b
sandy <san_dman@hotmail.com>
2024-06-07 17:04:17 +0200

    Add MAAAX

--- bot.js
+++ bot.js
@@ -10,6 +10,8 @@ const client = new Client({
   ],
 })
 
+let maxCounter = 1
+
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`)
 })
@@ -20,6 +22,20 @@ client.on('messageCreate', (message) => {
   if (message.stickers.hasAny('1199452550198460416')) {
     message.channel.send(`Best ${userMention(process.env.AEON)}!`)
     console.log('best aeon', Date())
+  }
+
+  if (/ma+x/i.test(message.content.toLowerCase())) {
+    message.channel.send(`M${'A'.repeat(maxCounter)}X ${userMention(process.env.MAX)}!`)
+    maxCounter++
+
+    if (maxCounter > 200) {
+      message.channel.send('MAXimum length reached, resetting!')
+    }
+  }
+
+  if (message.channel.id === process.env.RESETCHANNEL && message.content.toLowerCase().includes('reset')) {
+    message.channel.send('max?')
+    maxCounter = 1
   }
 })
 
