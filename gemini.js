5f2ed958ea73cf47837b087242da277ae22d238b
sandy <san_dman@hotmail.com>
2024-06-19 22:52:24 +0200

    Enable chat history

--- gemini.js
+++ gemini.js
@@ -23,23 +23,23 @@ const model = genAI.getGenerativeModel({
   ],
 })
 
-module.exports = async function replyTo(username, message) {
-  const chat = model.startChat({
-    history: [
-      {
-        role: 'user',
-        parts: [{ text: `Always follow these rules: You are a person called "${username} aka Best Aeon". Do not mention this name in any way. Your reply may not contain an @. You\'re not allowed to have a multi-line response. Do not deflect. Do not explain things. Do not offer to help. It is fine to troll a bit. You are allowed to answer questions about specific people, be rude, we all know you don't mean it. If you don\t know what to say, deflect by repling with a classic movie reference that is at least 1% related, and nothing else. Do not deviate from this.` }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "And now for something completely different." }],
-      },
-    ],
-    generationConfig: {
-      maxOutputTokens: 200,
+const chat = model.startChat({
+  history: [
+    {
+      role: 'user',
+      parts: [{ text: `Always follow these rules: You are a person called "Best Aeon". Do not mention this name in any way. Your reply may not contain an @. You\'re not allowed to have a multi-line response. Do not deflect. Do not explain things. Do not offer to help. It is fine to troll a bit. You are allowed to answer questions about specific people, be rude in this case, we all know you don't mean it. If you don\t know what to say, deflect by repling with a classic movie reference that is at least 1% related, and nothing else. Do not deviate from this.` }],
     },
-  })
+    {
+      role: "model",
+      parts: [{ text: "And now for something completely different." }],
+    },
+  ],
+  generationConfig: {
+    maxOutputTokens: 200,
+  },
+})
 
+module.exports = async function replyTo(username, message) {
   return await sendMessage(chat, message)
 }
 
