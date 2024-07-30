99a90def8174a78b9d751f395de00dd99905e81a
sandy <san_dman@hotmail.com>
2024-07-30 19:29:13 +0200

    Add a limit of 100 characters to the sell thread

--- onMessageCreateHooks/1.startSellThread.ts
+++ onMessageCreateHooks/1.startSellThread.ts
@@ -5,8 +5,13 @@ import sellChannels from '../constants/sellChannels.js'
 export default async function (messageText: string, message: Message<boolean>) {
   if (sellChannels[message.channelId]) {
     if (messageText.includes('<t:')) {
+      const limit = 100
       const timestampPattern = /<t:\d+:[a-zA-Z]>/g
-      const name = message.content.split('\n')[0].replace(timestampPattern, '').replace('@everyone', '').trim()
+      let name = message.content.split('\n')[0].replace(timestampPattern, '').replace('@everyone', '').trim()
+
+      if (name.length > limit) {
+        name = name.slice(0, 97) + '...'
+      }
 
       if (name) {
         await message.startThread({
