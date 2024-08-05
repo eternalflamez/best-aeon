04792d437c116497f3459822e97a087ce71499a7
Jan <jan100199@aol.com>
2024-08-05 19:54:35 +0200

    feat: add chat input command listener

---
+++ load-commands.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs')
+const path = require('node:path')
+const { Collection } = require('discord.js')
+
+export default function loadCommands(client) {
+  client.commands = new Collection()
+  const foldersPath = path.join(__dirname, 'commands')
+  const commandFolders = fs.readdirSync(foldersPath)
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder)
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js') || file.endsWith('.ts'))
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file)
+      const command = require(filePath)
+      if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command)
+      } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+      }
+    }
+  }
+}
