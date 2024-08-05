0f3e151f8f046b379251577244327b67b105b6bd
Jan <jan100199@aol.com>
2024-08-05 19:55:06 +0200

    feat: slash commands for backup, sellinfo and signoff

---
+++ commands/utility/backup.ts
@@ -0,0 +1,77 @@
+import { SlashCommandBuilder, ThreadChannel, Message, CommandInteraction } from 'discord.js'
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('backup')
+    .setDescription('Pings the backups for the corresponding sell. Only works in the sell thread.'),
+  async execute(interaction: CommandInteraction) {
+    if (!(interaction.channel instanceof ThreadChannel)) {
+      await interaction.reply({
+        content: 'This channel is not a thread! Command only works in the corresponding sell thread.',
+        ephemeral: true,
+      })
+      return
+    }
+
+    let originalMessage: Message | null = null
+    try {
+      originalMessage = await interaction.channel.fetchStarterMessage()
+    } catch (error) {
+      console.error('Error fetching starter message:', error)
+      await interaction.reply({
+        content: 'Failed to fetch the original message.',
+        ephemeral: true,
+      })
+    }
+    if (!originalMessage) {
+      return
+    }
+
+    const reactions = originalMessage.reactions.cache
+    const relevantEmojis = ['MCBU']
+    const filteredReactions = reactions.filter((reaction) => relevantEmojis.includes(reaction.emoji.name!))
+
+    if (filteredReactions.size === 0) {
+      await interaction.reply({
+        content: 'There are sadly no backups signed up for this sell.',
+        ephemeral: true,
+      })
+      return
+    }
+
+    let backupPeople: string[] = []
+
+    if (reactions.values())
+      for (const reaction of reactions.values()) {
+        const emoji = reaction.emoji.name
+        let users
+        try {
+          users = await reaction.users.fetch()
+        } catch (error) {
+          console.error('Error fetching users for reaction:', error)
+          await interaction.reply({
+            content: 'Failed to fetch users for a reaction.',
+            ephemeral: true,
+          })
+          return
+        }
+        switch (emoji) {
+          case 'MCBU':
+            backupPeople.push(...users.keys())
+            break
+          default:
+            break
+        }
+      }
+
+    let messageText = ''
+
+    messageText += `Aye, a backup is needed!\n${formatMentions(backupPeople)}`
+
+    await interaction.reply({ content: messageText, ephemeral: false })
+  },
+}
+
+function formatMentions(userIds: string[]) {
+  return userIds.map((id) => `<@${id}>`).join(' ')
+}
---
+++ commands/utility/sellinfo.ts
@@ -0,0 +1,99 @@
+import { SlashCommandBuilder, ThreadChannel, Message, CommandInteraction } from 'discord.js'
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('sellinfo')
+    .setDescription('Posts the sign-up for the corresponding sell. Only works in the sell thread.'),
+  async execute(interaction: CommandInteraction) {
+    const interactionUser = interaction.user.id
+    if (!(interaction.channel instanceof ThreadChannel)) {
+      await interaction.reply({
+        content: 'This channel is not a thread! Command only works in the corresponding sell thread.',
+        ephemeral: true,
+      })
+      return
+    }
+
+    let originalMessage: Message | null = null
+    try {
+      originalMessage = await interaction.channel.fetchStarterMessage()
+    } catch (error) {
+      console.error('Error fetching starter message:', error)
+      await interaction.reply({
+        content: 'Failed to fetch the original message.',
+        ephemeral: true,
+      })
+    }
+    if (!originalMessage) {
+      return
+    }
+
+    const reactions = originalMessage.reactions.cache
+    const relevantEmojis = ['MCMysticCoin', 'MCBU', 'MCUNSURE']
+    const filteredReactions = reactions.filter((reaction) => relevantEmojis.includes(reaction.emoji.name!))
+
+    if (filteredReactions.size === 0) {
+      await interaction.reply({
+        content: 'No relevant reactions found on the original message.',
+        ephemeral: true,
+      })
+      return
+    }
+
+    let signedPeople: string[] = []
+    let backupPeople: string[] = []
+    let unsurePeople: string[] = []
+
+    for (const reaction of reactions.values()) {
+      const emoji = reaction.emoji.name
+      let users
+      try {
+        users = await reaction.users.fetch()
+      } catch (error) {
+        console.error('Error fetching users for reaction:', error)
+        await interaction.reply({
+          content: 'Failed to fetch users for a reaction.',
+          ephemeral: true,
+        })
+        return
+      }
+      switch (emoji) {
+        case 'MCMysticCoin':
+          signedPeople.push(...users.keys())
+          break
+        case 'MCBU':
+          backupPeople.push(...users.keys())
+          break
+        case 'MCUNSURE':
+          unsurePeople.push(...users.keys())
+          break
+        default:
+          break
+      }
+    }
+
+    let messageText = ''
+
+    if (signedPeople.includes(interactionUser)) {
+      messageText += '**You are signed up!**\n'
+    } else if (backupPeople.includes(interactionUser)) {
+      messageText += '**You are signed up as a backup!**\n'
+    } else if (unsurePeople.includes(interactionUser)) {
+      messageText += '**You are signed up as unsure!**\n'
+    } else {
+      messageText += '**You are not signed up at all!**\n'
+    }
+    messageText += '\n'
+
+    messageText += `Main roster (${signedPeople.length}): ${formatMentions(signedPeople)}\n`
+    messageText += `Backup (${backupPeople.length}): ${formatMentions(backupPeople)}\n`
+    if (unsurePeople.length > 0) {
+      messageText += `Unsure (${unsurePeople.length}): ${formatMentions(unsurePeople)}\n`
+    }
+    await interaction.reply({ content: messageText, ephemeral: true })
+  },
+}
+
+function formatMentions(userIds: string[]) {
+  return userIds.map((id) => `<@${id}>`).join(', ')
+}
