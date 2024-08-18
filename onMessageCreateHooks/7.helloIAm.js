451a5a2eb1af086ac96e8e0b23f7fcb4c930dd44
sandy <san_dman@hotmail.com>
2024-08-18 13:23:18 +0200

    Exclude 332175471598895105 from hello I am

--- onMessageCreateHooks/7.helloIAm.js
+++ onMessageCreateHooks/7.helloIAm.js
@@ -1,7 +1,13 @@
-import { userMention } from "discord.js"
-import helloIAm from "./utility/helloIAm.js"
+import { userMention } from 'discord.js'
+import helloIAm from './utility/helloIAm.js'
+
+const exclusions = ['332175471598895105']
 
 export default async function (client, message) {
+  if (exclusions.includes(message.author.id)) {
+    return true
+  }
+
   const iAm = helloIAm(
     message.content.replace(/<@!?(\d+)>/g, userMention(message.author.id)),
     userMention(client.user.id),
