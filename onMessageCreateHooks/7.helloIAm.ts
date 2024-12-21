f7e353f3c10f1c5b38481d2ba10bb9f69b47ecf5
sandy <san_dman@hotmail.com>
2024-12-21 20:50:20 +0100

    Enable exclusion for hello I am

--- onMessageCreateHooks/7.helloIAm.ts
+++ onMessageCreateHooks/7.helloIAm.ts
@@ -2,7 +2,13 @@ import { Client, Message, userMention } from 'discord.js'
 import helloIAm from './utility/helloIAm.ts'
 import { logIAm } from '../firestore/log.ts'
 
+const exclusions = ['332175471598895105']
+
 export default async function (client: Client, message: Message) {
+  if (exclusions.includes(message.author.id)) {
+    return true
+  }
+
   const iAm = helloIAm(message.content, userMention(client.user!.id), message.author.id)
 
   if (iAm && Math.random() < 0.1) {
