703726172e856c0f1ada26b55c40bbb4cd548f34
sandy <san_dman@hotmail.com>
2025-03-29 15:55:32 +0100

    Clean up sell channels

--- constants/sellChannels.ts
+++ constants/sellChannels.ts
@@ -1,20 +1,12 @@
 const sellChannels = {
-  // instant-sells
-  '821737329215275039': { region: 'EU' },
-  // scheduled-raids
+  // scheduled-frogs
   '803274143311069204': { region: 'EU' },
-  // scheduled-strikes
+  // scheduled-cms
   '982039087663951892': { region: 'EU' },
-  // scheduled-fractals
-  '982039130047397988': { region: 'EU' },
-  // na-instant-sells
-  '1285719338371911832': { region: 'NA' },
-  // na-scheduled-raids
+  // na-scheduled-frogs
   '1196920908842008728': { region: 'NA' },
-  // na-scheduled-strikes
+  // na-scheduled-cms
   '1196923586791874640': { region: 'NA' },
-  // na-scheduled-fractals
-  '1285376242597625927': { region: 'NA' },
 
   // BTB
   // instant-sales
@@ -28,9 +20,7 @@ function isValidSellChannel(channelId: string): channelId is keyof typeof sellCh
 }
 
 function isInstantChannel(channelId: string) {
-  return (
-    channelId === '821737329215275039' || channelId === '1285719338371911832' || channelId === '1249829604974268418'
-  )
+  return channelId === '1249829604974268418'
 }
 
 function getRegion(channelid: string) {
