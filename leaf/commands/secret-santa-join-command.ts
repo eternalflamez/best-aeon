5d0d5366698b9ebd3eb91b93269cc920892a5921
sandy <san_dman@hotmail.com>
2025-12-22 17:39:44 +0100

    Update secret santa join command

--- leaf/commands/secret-santa-join-command.ts
+++ leaf/commands/secret-santa-join-command.ts
@@ -1,33 +1,11 @@
-import {
-  SlashCommandBuilder,
-  ChatInputCommandInteraction,
-  MessageFlags,
-  GuildMember,
-  userMention,
-  Client,
-} from 'discord.js'
-import leafDb from '../leaf-firestore.ts'
+import { SlashCommandBuilder, ChatInputCommandInteraction, MessageFlags, userMention } from 'discord.js'
 
 const command = {
   data: new SlashCommandBuilder().setName('join-secret-santa').setDescription('Join the Secret Santa!'),
-  async execute(interaction: ChatInputCommandInteraction, client: Client) {
+  async execute(interaction: ChatInputCommandInteraction) {
     try {
-      let displayName: string = (interaction.member as GuildMember)?.displayName
-
-      if (!displayName) {
-        displayName =
-          client.guilds.cache.get('798151793577558037')?.members.cache.get(interaction.user.id)?.displayName ||
-          interaction.user.displayName
-      }
-
-      await leafDb?.collection('santas').doc(interaction.user.id).set({
-        id: interaction.user.id,
-        display: displayName,
-      })
-
       await interaction.reply({
-        content:
-          "You succesfully joined the Secret Santa! <:leaf_helper:1433816388497309696> I will DM you with info on who you're paired with when we start!",
+        content: 'You are too late to join the Secret Santa! <:leaf_helper:1433816388497309696>',
         flags: MessageFlags.Ephemeral,
       })
     } catch (e) {
@@ -35,7 +13,7 @@ const command = {
       console.log(e)
 
       await interaction.reply({
-        content: `There was an error joining the Secret Santa! Feel free to try again, and/or poke my overlord ${userMention('109707866629246976')}.`,
+        content: `There was an error joining the Secret Santa! Feel free to try again even though the entry period has ended, and/or poke my overlord ${userMention('109707866629246976')}.`,
         flags: MessageFlags.Ephemeral,
       })
     }
