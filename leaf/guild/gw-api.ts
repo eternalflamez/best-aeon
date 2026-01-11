7fe964516886997a3a295f21077e2a74375da410
sandy <san_dman@hotmail.com>
2026-01-11 21:06:05 +0100

    Add getmembers call

--- leaf/guild/gw-api.ts
+++ leaf/guild/gw-api.ts
@@ -1,3 +1,7 @@
+import { config } from 'dotenv'
+
+config()
+
 export class GuildWarsData {
   private static cachedItems: { [id: string]: Item } = {}
 
@@ -25,6 +29,23 @@ export class GuildWarsData {
 
     return item
   }
+
+  public static async getMembers(): Promise<GW2Member[]> {
+    const response = await fetch(`https://api.guildwars2.com/v2/guild/${process.env.LEAF_GUILD_ID}/members`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.LEAF_GUILD_BEARER}`,
+      },
+    })
+
+    if (!response.ok) {
+      console.log(`Error fetching item - ${response.status}`)
+
+      return []
+    }
+
+    return (await response.json()) as GW2Member[]
+  }
 }
 
 interface Item {
@@ -32,4 +53,11 @@ interface Item {
   name: string
   chat_link: string
   icon?: string
+}
+
+interface GW2Member {
+  name: string
+  rank: string
+  joined: string
+  wvw_member: boolean
 }
