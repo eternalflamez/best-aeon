d33280361b36c4b8b5da86f60c15b90e74aa01b5
sandy <san_dman@hotmail.com>
2026-02-27 22:23:59 +0100

    Disable bot startups from dev

--- startup-dev.ts
+++ startup-dev.ts
@@ -12,25 +12,25 @@ config()
 
 const clientId = uuidv4()
 
-SetupBuyerManagement({
-  botClientId: clientId,
-  guildTag: 'Rice',
-  guildId: '1248337933413650614',
-  managerToken: process.env.MANAGER_TOKEN!,
-  contactedCategoryChannelId: '1269279315590250600',
-  buyerManagementChannelId: '1263780410089799726',
-  previousBuyersChannelId: '1271422645606285374',
-  priceEmbedChannelId: '1426138997415219301',
-  embedRaidBoss: 'DuGz46p6Ccx9ZkOd4F1T',
-  embedRaidAchievements: 'IKlrMn3nF739xn2CO93i',
-  embedFractals: 'JCqiHCV1lxMjVxvmfUgD',
-  embedStrikes: 'cALy5pc91wyaam0kK6MJ',
-  botRoleId: '1264610829811056718',
-  translations: riseTranslations,
-})
+// SetupBuyerManagement({
+//   botClientId: clientId,
+//   guildTag: 'Rice',
+//   guildId: '1248337933413650614',
+//   managerToken: process.env.MANAGER_TOKEN!,
+//   contactedCategoryChannelId: '1269279315590250600',
+//   buyerManagementChannelId: '1263780410089799726',
+//   previousBuyersChannelId: '1271422645606285374',
+//   priceEmbedChannelId: '1426138997415219301',
+//   embedRaidBoss: 'DuGz46p6Ccx9ZkOd4F1T',
+//   embedRaidAchievements: 'IKlrMn3nF739xn2CO93i',
+//   embedFractals: 'JCqiHCV1lxMjVxvmfUgD',
+//   embedStrikes: 'cALy5pc91wyaam0kK6MJ',
+//   botRoleId: '1264610829811056718',
+//   translations: riseTranslations,
+// })
 
-startup(clientId)
+// startup(clientId)
 
-startupLeaf(clientId)
+// startupLeaf(clientId)
 
 setupApi()
