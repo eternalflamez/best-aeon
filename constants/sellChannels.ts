e94f0fd8f1db2af8026c576dd03fc4c9fd4540e7
sandy <san_dman@hotmail.com>
2026-02-28 15:21:49 +0100

    Remove deleted channels

--- constants/sellChannels.ts
+++ constants/sellChannels.ts
@@ -1,12 +1,8 @@
 const sellChannels = {
-  // scheduled-frogs
-  '803274143311069204': { region: 'EU' },
   // scheduled-cms
   '982039087663951892': { region: 'EU' },
   // scheduled-practise
   '1444445198758187071': { region: 'EU' },
-  // na-scheduled-frogs
-  '1196920908842008728': { region: 'NA' },
   // na-scheduled-cms
   '1196923586791874640': { region: 'NA' },
 
